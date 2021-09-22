bb6ccd1360ea5d5ccc38ebb1d6a20b5716c62040
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2021-09-22 14:08:30 -0700

    add example for defaultComponent

--- packages/react/example/src/App.jsx
+++ packages/react/example/src/App.jsx
@@ -8,6 +8,11 @@ function App() {
       <header className="App-header">
         <Davatar size={120} address={'0x9B6568d72A6f6269049Fac3998d1fadf1E6263cc'} />
         <Davatar size={120} address={'0x9B6568d72A6f6269049Fac3998d1fadf1E6263cc'} generatedAvatarType="blockies" />
+        <Davatar
+          size={120}
+          address={'0x9B6568d72A6f6269049Fac3998d1fadf1E6263cc'}
+          defaultComponent={<h2>Loading...</h2>}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
