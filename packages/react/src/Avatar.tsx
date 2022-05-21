a73672d63d3337695f5e1763189efede7d89c989
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-05-21 19:02:06 -0400

    add docstrings

--- packages/react/src/Avatar.tsx
+++ packages/react/src/Avatar.tsx
@@ -4,26 +4,40 @@ import React, { useEffect, useState, ReactChild, CSSProperties } from 'react';
 import Image from './Image';
 
 export type AvatarProps = {
+  /**
+   * The size of the avatar in pixels.
+   */
   size: number;
+  /**
+   * An Ethereum address
+   */
   address: string;
+  /**
+   * An ethers or web3.js provider
+   */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   provider?: any;
-  // deprecated
+  /**
+   * An API key for The Graph
+   *
+   * @deprecated
+   */
   graphApiKey?: string;
+  /**
+   * The kind of generated avatar to display if no avatar
+   */
   generatedAvatarType?: 'jazzicon' | 'blockies';
+  /**
+   * A default component to render if there is no avatar
+   */
   defaultComponent?: ReactChild | ReactChild[];
+  /**
+   * Custom CSS styles to apply to the avatar <img /> tag
+   */
   style?: CSSProperties;
 };
 
-export default function Avatar({
-  size,
-  address,
-  provider,
-  graphApiKey,
-  generatedAvatarType,
-  defaultComponent,
-  style,
-}: AvatarProps) {
+export default function Avatar({ size, address, provider, generatedAvatarType, defaultComponent, style }: AvatarProps) {
   const [avatarUri, setAvatarUri] = useState<string | null>(null);
   const [ethersProvider, setEthersProvider] = useState<BaseProvider | null>(null);
 
@@ -68,7 +82,6 @@ export default function Avatar({
       size={size}
       address={address}
       uri={avatarUri}
-      graphApiKey={graphApiKey}
       provider={ethersProvider}
       generatedAvatarType={generatedAvatarType}
       defaultComponent={defaultComponent}
--- packages/react/src/Image.tsx
+++ packages/react/src/Image.tsx
@@ -20,15 +20,40 @@ const erc1155Abi = [
 ];
 
 export type ImageProps = {
+  /**
+   * The size of the avatar in pixels.
+   */
   size: number;
+  /**
+   * The URI of the image to be resolved
+   */
   uri?: string | null;
+  /**
+   * An Ethereum address
+   */
   address?: string | null;
+  /**
+   * Custom CSS styles to apply to the <img /> tag
+   */
   style?: CSSProperties;
   className?: string;
-  // deprecated
+  /**
+   * An API key for The Graph
+   *
+   * @deprecated
+   */
   graphApiKey?: string;
+  /**
+   * An ethers or web3.js provider
+   */
   provider?: BaseProvider | null;
+  /**
+   * The kind of generated avatar to display if image isn't loaded
+   */
   generatedAvatarType?: 'jazzicon' | 'blockies';
+  /**
+   * A default component to render if image isn't loaded
+   */
   defaultComponent?: ReactChild | ReactChild[];
 };
 
